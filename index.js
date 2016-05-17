c55eb24e8a34c639458c329548be95ce98b4a790
Joao Felipe <joao.felipe@concretesolutions.com.br>
2016-05-17 13:37:26 -0300

    trocando de buzzer pra led, e fazendo o mesmo permanecer ligado por 1 minuto quando detectar presença, assim desconsiderando as leituras neste periodo

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var express    = require('express');
 var app        = express();
 var port = process.env.PORT || 8080; 
-
+var delay = 1000;
 var Gpio = require('onoff').Gpio,
-  buzzer = new Gpio(4, 'out'),
+  led = new Gpio(4, 'out'),
   pir = new Gpio(17, 'in'),
   iv;
 
@@ -22,14 +22,25 @@ console.log('Magic happens on port ' + port);
 
 iv = setInterval(function () {
   state = pir.readSync() ^ 0; // 1 = on 0 = off
-  // buzzer.writeSync(state);
+  toggleLed(state);
   console.log("curr state:" + state);
-}, 2000);
+}, delay);
 
 function exit() {
-  buzzer.unexport();
+  led.unexport();
   pir.unexport();
   process.exit();
+};
+
+function toggleLed(state) {
+ if(state === 1) {
+   led.writeSync(1);
+   setTimeout(offLed,60000);
+ }
+};
+
+function offLed(){
+ led.writeSync(0);
 };
 
 process.on('SIGINT', exit);
