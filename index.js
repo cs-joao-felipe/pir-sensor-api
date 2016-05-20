c500889f6be9e2ddb154cb595add3d44fb4f4eb1
alan.silva <alan.silva@concretesolutions.com.br>
2016-05-20 20:31:41 -0300

    Refactored index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-var readCount= 0;
-var express    = require('express');
-var app        = express();
-var port = process.env.PORT || 8080; 
+'use strict';
+
+var readCount = 0;
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
 var delay = 1000;
 var timeout;
 var Gpio = require('onoff').Gpio,
@@ -10,17 +12,11 @@ var Gpio = require('onoff').Gpio,
   iv;
 
 var state = 0;
+function getState() {
+    return state;
+}
 
-// routings
-var router = express.Router();
-router.route('/pir/status')
-    .get(function(req, res) {
-	res.json({ occupied: state === 1 });
-    });
-
-app.use('/api', router);
-app.listen(port);
-console.log('Magic happens on port ' + port);
+require('./lib/route')(app, getState);
 
 iv = setInterval(function () {
   toggleState(pir.readSync() ^ 0); // 1 = on 0 = off
@@ -29,26 +25,39 @@ iv = setInterval(function () {
 }, delay);
 
 function exit() {
-  led.unexport();
-  pir.unexport();
-  process.exit();
+    led.unexport();
+    pir.unexport();
+    process.exit();
 };
 
 function toggleState(newState) {
- if(newState === 1) {
-   led.writeSync(1);
-   if(timeout) {
-     console.log("clearing previous timeout.");
-     clearTimeout(timeout);
-   }
-   state = 1;
-   timeout = setTimeout(offLed,60000);
- }
-};
+    if(newState !== 1) return;
 
-function offLed(){
- led.writeSync(0);
- state = 0;
-};
+    turnLedOn();
+
+    if (timeout) {
+        console.log("clearing previous timeout.");
+        clearTimeout(timeout);
+    }
+
+    timeout = setTimeout(turnLedOff, 60000);
+}
+
+function turnLedOff(){
+    led.writeSync(0);
+    state = 0;
+
+    console.log('Led had just turned off');
+}
+
+function turnLedOn() {
+    led.writeSync(1);
+    state = 1;
+
+    console.log('Led ');
+}
 
 process.on('SIGINT', exit);
+
+app.listen(port);
+console.log(`Magic happens on port ${ port }`);
